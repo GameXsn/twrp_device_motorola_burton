b27d41b066077cb33af096c44e1c4ca5d77e3421
GameXsn <gamexgameplay1@gmail.com>
2025-01-23 13:57:10 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_burton.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_burton-user \
-    twrp_burton-userdebug \
-    twrp_burton-eng
+COMMON_LUNCH_CHOICES := twrp_burton-user \ twrp_burton-eng
